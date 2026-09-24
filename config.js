c44da39e3c6409feecbbceb2a53c520379b28d02
Man1ek734 <p4staniek@gmail.com>
2026-09-24 21:48:10 +0200

    Connect website to Supabase database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Ten plik może być publiczny na GitHub Pages.
 // SUPABASE_ANON_KEY jest kluczem publicznym. NIGDY nie wpisuj tutaj SERVICE_ROLE_KEY.
 window.LSSD_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://xeyvlzvfojzuzuqabciw.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_BgsfR0I7HG0HVlfToT808A_Y-NWwWUt"
 };
