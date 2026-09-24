6a849f132b77cea2652d4d1f5d64c380f9a4c00b
Man1ek734 <p4staniek@gmail.com>
2026-09-24 20:26:17 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Ten plik może być publiczny na GitHub Pages.
+// SUPABASE_ANON_KEY jest kluczem publicznym. NIGDY nie wpisuj tutaj SERVICE_ROLE_KEY.
+window.LSSD_CONFIG = {
+  SUPABASE_URL: "",
+  SUPABASE_ANON_KEY: ""
+};
