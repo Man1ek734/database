4e6773b45305fac6878e01e4a73011065fe0e9a1
Man1ek734 <p4staniek@gmail.com>
2026-09-24 22:09:34 +0200

    Add Discord auth API URL

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 // SUPABASE_ANON_KEY jest kluczem publicznym. NIGDY nie wpisuj tutaj SERVICE_ROLE_KEY.
 window.LSSD_CONFIG = {
   SUPABASE_URL: "https://xeyvlzvfojzuzuqabciw.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_BgsfR0I7HG0HVlfToT808A_Y-NWwWUt"
+  SUPABASE_ANON_KEY: "sb_publishable_BgsfR0I7HG0HVlfToT808A_Y-NWwWUt",
+  API_URL: "https://lssd-discord-bot-production.up.railway.app"
 };
